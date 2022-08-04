ffe68a4aea3348a1306ff7532b6a0370c14387c1
Vince Weaver <vince@deater.net>
2022-08-04 00:16:05 -0400

    utils: update playfield generation code

--- utils/png2pf.c
+++ utils/png2pf.c
@@ -15,13 +15,30 @@ static unsigned char reverse_byte(unsigned char b) {
 }
 
 static void print_help(char *name) {
-	fprintf(stderr,"Usage:\t%s [-2] [-4] [-8] [-b] INFILE OUTFILE\n\n",name);
-	fprintf(stderr,"\t-b : generate background color data\n");
+	fprintf(stderr,"Usage:\t%s [-2] [-4] [-8] [-b col] [-n name] INFILE OUTFILE\n\n",name);
+	fprintf(stderr,"\t-g : generate background color data\n");
 	fprintf(stderr,"\t-2 : only draw every 2nd line\n");
 	fprintf(stderr,"\t-4 : only draw every 4th line\n");
 	fprintf(stderr,"\t-8 : only draw every 8th line\n");
+	fprintf(stderr,"\t-n : name to prepend to labels\n");
+	fprintf(stderr,"\t-b : color to use for background (default 0)\n");
 	exit(-1);
 }
+
+
+static void print_byte(FILE *outfile,int value,int which) {
+
+	if (((which)%8)==0) {
+		fprintf(outfile,"\t.byte $%02X",value);
+	}
+	else {
+		fprintf(outfile,",$%02X",value);
+	}
+	if (((which)%8)==7) {
+		fprintf(outfile,"\n");
+	}
+}
+
 
 /* Convert a 320x192 PNG to a 40x192 playfield with one color per line */
 
@@ -30,21 +47,28 @@ int main(int argc, char **argv) {
 	int row=0;
 	int col=0;
 	int skip=1;
-	int color;
+	int color,value,background_color=0;
 	int playfield_left[192],playfield_right[192],background[192];
 	int c,debug=0;
 	int generate_bg=0;
 
 	char input_filename[BUFSIZ],output_filename[BUFSIZ];
+	char *name=NULL;
 	unsigned char *image;
 	int xsize,ysize;
 	FILE *outfile;
 
 
 	/* Check command line arguments */
-	while ((c = getopt (argc, argv,"248bdhv"))!=-1) {
+	while ((c = getopt (argc, argv,"248b:gdhvn:"))!=-1) {
 		switch (c) {
+		case 'n':
+			name=strdup(optarg);
+			break;
 		case 'b':
+			background_color=strtod(optarg,NULL);
+			break;
+		case 'g':
 			generate_bg=1;
 			break;
 		case 'd':
@@ -84,14 +108,20 @@ int main(int argc, char **argv) {
 		return -1;
 	}
 
-	/* Grab output filename */
-	strncpy(output_filename,argv[optind],BUFSIZ-1);
+	if (argv[optind][0]=='-') {
+		outfile=stdout;
+	}
+	else {
+
+		/* Grab output filename */
+		strncpy(output_filename,argv[optind],BUFSIZ-1);
 
 
-	outfile=fopen(output_filename,"w");
-	if (outfile==NULL) {
-		fprintf(stderr,"Error!  Could not open %s\n",output_filename);
-		exit(-1);
+		outfile=fopen(output_filename,"w");
+		if (outfile==NULL) {
+			fprintf(stderr,"Error!  Could not open %s\n",output_filename);
+			exit(-1);
+		}
 	}
 
 	if (loadpng(input_filename,&image,&xsize,&ysize,PNG_WHOLETHING,8)<0) {
@@ -107,7 +137,9 @@ int main(int argc, char **argv) {
 		return -1;
 	}
 
-	for(row=0;row<ysize;row++) background[row]=0;
+	fprintf(outfile,"; Using background color $%02X\n",background_color);
+
+	for(row=0;row<ysize;row++) background[row]=background_color;
 
 	/* generate background color table */
 	if (generate_bg) {
@@ -120,14 +152,20 @@ int main(int argc, char **argv) {
 
 
 	/* generate color table */
-	fprintf(outfile,"colors:\n");
+	if (name) {
+		fprintf(outfile,"%s_colors:\n",name);
+	}
+	else {
+		fprintf(outfile,"colors:\n");
+	}
 	for(row=0;row<ysize;row+=skip) {
 		color=0;
 		for(col=0;col<xsize;col++) {
 			if (image[row*xsize+col]!=background[row]) color=image[row*xsize+col];
 		}
-		fprintf(outfile,"\t.byte $%02X\n",color);
+		print_byte(outfile,color,row/skip);
 	}
+	fprintf(outfile,"\n");
 
 	/* generate left playfield table */
 	for(row=0;row<ysize;row++) {
@@ -138,22 +176,34 @@ int main(int argc, char **argv) {
 		}
 	}
 
-	fprintf(outfile,"\nplayfield0_left:\n");
+	if (name) {
+		fprintf(outfile,"\n%s_playfield0_left:\n",name);
+	} else {
+		fprintf(outfile,"\nplayfield0_left:\n");
+	}
 	for(row=0;row<ysize;row+=skip) {
-		fprintf(outfile,"\t.byte $%02X\n",
-			reverse_byte((playfield_left[row]>>16)&0xff));
+		value=reverse_byte((playfield_left[row]>>16)&0xff);
+		print_byte(outfile,value,row/skip);
 	}
 
-	fprintf(outfile,"\nplayfield1_left:\n");
+	if (name) {
+		fprintf(outfile,"\n%s_playfield1_left:\n",name);
+	} else {
+		fprintf(outfile,"\nplayfield1_left:\n");
+	}
 	for(row=0;row<ysize;row+=skip) {
-		fprintf(outfile,"\t.byte $%02X\n",
-			(playfield_left[row]>>8)&0xff);
+		value=(playfield_left[row]>>8)&0xff;
+		print_byte(outfile,value,row/skip);
 	}
 
-	fprintf(outfile,"\nplayfield2_left:\n");
+	if (name) {
+		fprintf(outfile,"\n%s_playfield2_left:\n",name);
+	} else {
+		fprintf(outfile,"\nplayfield2_left:\n");
+	}
 	for(row=0;row<ysize;row+=skip) {
-		fprintf(outfile,"\t.byte $%02X\n",
-			reverse_byte((playfield_left[row]>>0)&0xff));
+		value=reverse_byte((playfield_left[row]>>0)&0xff);
+		print_byte(outfile,value,row/skip);
 	}
 
 
@@ -164,24 +214,40 @@ int main(int argc, char **argv) {
 			playfield_right[row]<<=1;
 			if (image[row*xsize+col+20]!=background[row]) playfield_right[row]|=1;
 		}
+//		fprintf(outfile,"row=%d bg=%x %x\n",row,background[row],playfield_right[row]);
 	}
 
-	fprintf(outfile,"\nplayfield0_right:\n");
+	if (name) {
+		fprintf(outfile,"\n%s_playfield0_right:\n",name);
+	} else {
+		fprintf(outfile,"\nplayfield0_right:\n");
+	}
 	for(row=0;row<ysize;row+=skip) {
-		fprintf(outfile,"\t.byte $%02X\n",
-			reverse_byte((playfield_right[row]>>16)&0xff));
+		value=reverse_byte((playfield_right[row]>>16)&0xff);
+		print_byte(outfile,value,row/skip);
 	}
 
-	fprintf(outfile,"\nplayfield1_right:\n");
+	fprintf(outfile,"\n.align $100\n");
+
+	if (name) {
+		fprintf(outfile,"\n%s_playfield1_right:\n",name);
+	} else {
+		fprintf(outfile,"\nplayfield1_right:\n");
+	}
 	for(row=0;row<ysize;row+=skip) {
-		fprintf(outfile,"\t.byte $%02X\n",
-			(playfield_right[row]>>8)&0xff);
+		value=(playfield_right[row]>>8)&0xff;
+		print_byte(outfile,value,row/skip);
 	}
 
-	fprintf(outfile,"\nplayfield2_right:\n");
+
+	if (name) {
+		fprintf(outfile,"\n%s_playfield2_right:\n",name);
+	} else {
+		fprintf(outfile,"\nplayfield2_right:\n");
+	}
 	for(row=0;row<ysize;row+=skip) {
-		fprintf(outfile,"\t.byte $%02X\n",
-			reverse_byte((playfield_right[row]>>0)&0xff));
+		value=reverse_byte((playfield_right[row]>>0)&0xff);
+		print_byte(outfile,value,row/skip);
 	}
 
 	fclose(outfile);
