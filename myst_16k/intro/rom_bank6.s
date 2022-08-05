e2993ebf76df1e5b37ed7ec186c60708d1c16359
Vince Weaver <vincent.weaver@maine.edu>
2022-08-05 12:43:55 -0400

    myst16: full intro re-hooked up

--- myst_16k/intro/rom_bank6.s
+++ myst_16k/intro/rom_bank6.s
@@ -8,12 +8,20 @@ do_intro:
 
 .include "title.s"
 
+.include "cleft.s"
+
+.include "book.s"
 
 	rts
 
 
 .include "../common_routines.s"
+.include "../hand_copy.s"
+.include "../hand_motion.s"
+.include "../adjust_sprite.s"
+
 
 .align $100
 
 .include "intro_data.inc"
+.include "../sprite_data.inc"
