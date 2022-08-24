6529d3f4dce09d665cf49bc4c4596010f3dffe79
Vince Weaver <vince@deater.net>
2022-08-24 19:07:57 -0400

    rr: use mostly our own code in the main loop now

--- rr/rr_scene.s
+++ rr/rr_scene.s
@@ -1,109 +1,113 @@
 
 ; =====================================================================
-; Start of code
-; =====================================================================
-
-
-; =====================================================================
 ; Initialize music.
 ; Set tt_cur_pat_index_c0/1 to the indexes of the first patterns from
 ; tt_SequenceTable for each channel.
 ; Set tt_timer and tt_cur_note_index_c0/1 to 0.
 ; All other variables can start with any value.
 ; =====================================================================
-        lda #0
-        sta tt_cur_pat_index_c0
-        lda #12
-        sta tt_cur_pat_index_c1
-        ; the rest should be 0 already from startup code. If not,
-        ; set the following variables to 0 manually:
-        ; - tt_timer
-        ; - tt_cur_pat_index_c0
-        ; - tt_cur_pat_index_c1
-        ; - tt_cur_note_index_c0
-        ; - tt_cur_note_index_c1
 
+	lda	#0
+	sta	tt_cur_pat_index_c0
+	lda	#12
+	sta	tt_cur_pat_index_c1
 
+	; the rest should be 0 already from startup code. If not,
+	; set the following variables to 0 manually:
+	; - tt_timer
+	; - tt_cur_pat_index_c0
+	; - tt_cur_pat_index_c1
+	; - tt_cur_note_index_c0
+	; - tt_cur_note_index_c1
 
-; =====================================================================
+;======================================================================
 ; MAIN LOOP
-; =====================================================================
+;======================================================================
 
-MainLoop:
+rr_frame:
+
+	;========================
+	; start VBLANK
+	;========================
+	; in scanline 0
+
+	jsr	common_vblank
 
 
+	;=======================
+	; 37 lines of VBLANK
+	;=======================
 
-; ---------------------------------------------------------------------
-; VBlank
-; ---------------------------------------------------------------------
 
-VBlank:
+	;========================
+	; update music player
+	;========================
+	; worst case for this particular song seems to be
+	;	around 9 * 64 = 576 cycles = 8 scanlines
+	;	make it 12 * 64 = 11 scanlines to be safe
 
-	lda	#%1110
-vsyncLoop:
-	sta	WSYNC
-	sta	VSYNC
-	lsr
-	bne vsyncLoop
+	; Original is 43 * 64 cycles = 2752/76 = 36.2 scanlines
 
-	lda	#2
-	sta	VBLANK
-	lda	#TIM_VBLANK
+	lda	#12		; TIM_VBLANK
 	sta	TIM64T
 
-        ; Do VBlank stuff
+
+	;=======================
+	; run the music player
+	;=======================
+
 .include "rr_player.s"
 
 	; Measure player worst case timing
-	lda	#TIM_VBLANK
+	lda	#12		; TIM_VBLANK
 	sec
 	sbc	INTIM
 	cmp	player_time_max
-	bcc	noNewMax
+	bcc	no_new_max
 	sta	player_time_max
-noNewMax:
+no_new_max:
 
 
-waitForVBlank:
-        lda INTIM
-        bne waitForVBlank
-        sta WSYNC
-        sta VBLANK
-
-
-; ---------------------------------------------------------------------
-; Kernel
-; ---------------------------------------------------------------------
-
-Kernel:
-        lda	#TIM_KERNEL
-        sta	T1024T
-
-        ; Do kernel stuff
-
-waitForIntim2:
+wait_for_vblank:
         lda	INTIM
-	bne	waitForIntim2
+        bne	wait_for_vblank
+
+	; in theory we are 10 scanlines in, need to delay 27 more
+
+	ldx	#27
+le_vblank_loop:
+	sta     WSYNC
+	dex
+	bne	le_vblank_loop
 
 
-; ---------------------------------------------------------------------
-; Overscan
-; ---------------------------------------------------------------------
-
-Overscan: ;        SUBROUTINE
-
-        sta	WSYNC
-        lda	#2
-        sta	VBLANK
-        lda	#TIM_OVERSCAN
-        sta	TIM64T
-
-        ; Do overscan stuff
-
-waitForIntim:
-        lda	INTIM
-        bne	waitForIntim
+	; one extra, for luck?  Not sure where my math is off
+	sta	WSYNC
 
 
-	jmp	MainLoop
+	;=========================
+	;=========================
+	; kernel
+	;=========================
+	;=========================
+	; 192 cycles
+
+	ldx	#192
+kernel_loop:
+	sta	WSYNC
+	dex
+	bne	kernel_loop
+
+
+
+	;===========================
+	;===========================
+	; overscan (30 cycles)
+	;===========================
+	;===========================
+
+	ldx     #30
+	jsr	common_overscan
+
+	jmp	rr_frame
 
