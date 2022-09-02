7c41da899d8c088f0a0e3efac50c333f16d0b1bb
Vince Weaver <vince@deater.net>
2022-09-01 23:26:45 -0400

    sc: update init_level
    make more deterministic, get accurate count

--- sc/init_level.s
+++ sc/init_level.s
@@ -1,8 +1,8 @@
-; NEEDS CYCLE RECOUNT
 
 	;============================
 	; init level
 	;============================
+	;
 
 
 init_level:
@@ -23,24 +23,18 @@ init_level:
 	lsr								; 2
 	lsr			; get LEVEL/8				; 2
 	tax								; 2
-	lda	#40		; 20 seconds				; 2
-; 27
-hardness_loop:
-	lsr								; 2
-	dex								; 2
-	bpl	hardness_loop						; 2/3
-
+	lda	hardness_lookup,X					; 4+
 	sta	TIME							; 3
+; 32
 
 	lda	#59							; 2
 	sta	TIME_SUBSECOND						; 3
-; ?+5
+; 37
 	lda	#0							; 2
 	sta	FRAME							; 3
 	sta	LEVEL_OVER						; 3
 	sta	DIDNT_TOUCH_WALL					; 3
-; ?+18
-
+; 48
 
 
 copy_level_data_in:
@@ -56,49 +50,60 @@ copy_level_data_in:
 	adc	#>level1_data						; 2
 	sta	INH							; 3
 	ldy	#7							; 2
-; 33
+								;==========
+								;	27
+; 75
 
 copy_level_data_loop:
-	lda	(INL),Y							; 5
+	lda	(INL),Y							; 5+
 	sta	LEVEL_INFO,Y						; 5
 	dey								; 2
 	bpl	copy_level_data_loop					; 2/3
 								;=============
-								; (16*15)-1
-								; 	239
+								; (8*15)-1
+								; 	119
+
+; 194
 
 
 	;========================
 	; adjust secret position
 
-	lda	SECRET_Y_START
-	clc
-	adc	#8
-	sta	SECRET_Y_END
+	lda	SECRET_Y_START						; 3
+	clc								; 2
+	adc	#8							; 2
+	sta	SECRET_Y_END						; 3
+; 204
 
-	lda	SECRET_X_START
+	lda	SECRET_X_START						; 3
 	lsr								; 2
 	lsr								; 2
 	lsr								; 2
 	lsr								; 2
 	sta	SECRET_X_COARSE						; 3
-; 16
+; 218
 	; apply fine adjust
 	lda	SECRET_X_START						; 3
 	and	#$0f							; 2
 	tax								; 2
 	lda	fine_adjust_table,X					; 4+
 	sta	SECRET_X_FINE						; 3
-; 30
+; 232
 
-	; init balls
-	lda	#0
-	ldx	SPEED
-	cpx	#3
-	bcs	too_many_balls
-	lda	#1
+	;===========================
+	; init speed boost
+	;	limit to 3
+
+	ldx	#0							; 2
+	lda	SPEED							; 3
+	cmp	#3							; 2
+	bcs	too_many_balls						; 2/3
+	inx								; 2
 too_many_balls:
-	sta	BALL_OUT
+	stx	BALL_OUT						; 3
+								;===========
+								; 14/13
+; 246
 
 reinit_strongbad:
 	; adjust strong bad position
@@ -113,7 +118,15 @@ reinit_strongbad:
 	sta	STRONGBAD_Y_LOW						; 3
 	sta	STRONGBAD_ON
 
-; 61 (79)
-	rts
+; 264
 
+	rts								; 6
 
+; 270 = ~3.5 scanlines
+
+; could use some tuning?
+; amount of time allowed for each wave
+; in theory anything less than 2 probably not humanly possible
+
+hardness_lookup:
+	.byte 20,10,5,4,3,2,1,0
