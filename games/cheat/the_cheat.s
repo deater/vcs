13d6fb872787eb0d3f681fa5aee5e3fe3645c7d9
Vince Weaver <vince@deater.net>
2023-03-20 19:06:02 -0400

    cheat: consolidate data so we can pack it better

--- games/cheat/the_cheat.s
+++ games/cheat/the_cheat.s
@@ -51,19 +51,21 @@ scanline_wait:
 
 .align $100
 
-.include "strongbadia.inc"
-.include "blue.inc"
-.include "pit.inc"
-.include "the_stick.inc"
-.include "sfx.inc"
+.include "game_data.inc"
+
+;.include "strongbadia.inc"
+;.include "blue.inc"
+;.include "pit.inc"
+;.include "the_stick.inc"
+;.include "sfx.inc"
 
 
-score_bitmap0:  ; turns out this was same pattern
-score_zeros:    .byte $22,$55,$55,$55,$55,$55,$22,$00
-score_ones:     .byte $22,$66,$22,$22,$22,$22,$77,$00
-score_twos:     .byte $22,$55,$11,$22,$44,$44,$77,$00
-score_threes:   .byte $22,$55,$11,$22,$11,$55,$22,$00
-score_fours:    .byte $55,$55,$55,$77,$11,$11,$11,$00
+;score_bitmap0:  ; turns out this was same pattern
+;score_zeros:    .byte $22,$55,$55,$55,$55,$55,$22,$00
+;score_ones:     .byte $22,$66,$22,$22,$22,$22,$77,$00
+;score_twos:     .byte $22,$55,$11,$22,$44,$44,$77,$00
+;score_threes:   .byte $22,$55,$11,$22,$11,$55,$22,$00
+;score_fours:    .byte $55,$55,$55,$77,$11,$11,$11,$00
 
 ; --****** ----**-- --****** ----**-- ----**--
 ; --**---- --**--** ------** --**--** --**--**
@@ -73,37 +75,37 @@ score_fours:    .byte $55,$55,$55,$77,$11,$11,$11,$00
 ; --**--** --**--** --**---- --**--** --**--**
 ; ----**-- ----**-- --**---- ----**-- ----**--
 
-score_fives:    .byte   $77,$44,$66,$11,$11,$55,$22,$00
-score_sixes:    .byte   $33,$44,$44,$66,$55,$55,$22,$00
-score_sevens:   .byte   $77,$11,$11,$22,$44,$44,$44,$00
-score_eights:   .byte   $22,$55,$55,$22,$55,$55,$22,$00
-score_nines:    .byte   $22,$55,$55,$33,$11,$55,$22,$00
+;score_fives:    .byte   $77,$44,$66,$11,$11,$55,$22,$00
+;score_sixes:    .byte   $33,$44,$44,$66,$55,$55,$22,$00
+;score_sevens:   .byte   $77,$11,$11,$22,$44,$44,$44,$00
+;score_eights:   .byte   $22,$55,$55,$22,$55,$55,$22,$00
+;score_nines:    .byte   $22,$55,$55,$33,$11,$55,$22,$00
 
-cheat_sprite_yellow:
-	.byte $00
-	.byte $00
-	.byte $7D
-	.byte $3F
-	.byte $7E
-	.byte $7F
-	.byte $7D
-	.byte $7F
-	.byte $3E
-	.byte $57
-	.byte $FC
+;cheat_sprite_yellow:
+;	.byte $00
+;	.byte $00
+;	.byte $7D
+;	.byte $3F
+;	.byte $7E
+;	.byte $7F
+;	.byte $7D
+;	.byte $7F
+;	.byte $3E
+;	.byte $57
+;	.byte $FC
 
-cheat_sprite_black:
-	.byte $00
-	.byte $00
-	.byte $0A
-	.byte $00
-	.byte $04
-	.byte $01
-	.byte $0B
-	.byte $02
-	.byte $04
-	.byte $A2
-	.byte $04
+;cheat_sprite_black:
+;	.byte $00
+;	.byte $00
+;	.byte $0A
+;	.byte $00
+;	.byte $04
+;	.byte $01
+;	.byte $0B
+;	.byte $02
+;	.byte $04
+;	.byte $A2
+;	.byte $04
 
 
 .segment "IRQ_VECTORS"
