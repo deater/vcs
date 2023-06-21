c271f20236494a0827541eaade671b3c7ef13638
Vince Weaver <vincent.weaver@maine.edu>
2023-06-21 13:23:35 -0400

    myst: looking into it again

--- games/myst_16k/myst.s
+++ games/myst_16k/myst.s
@@ -1,10 +1,13 @@
 ; Myst for Atari 2600
 
+; this creates the final ROM image after segments were
+;	created separtely
+
 ; For E7 bank-switched cartridge (16k ROM, 2k RAM)
 
 ;	1FE0 - 1FE6 activates that bank of ROM into $1000-$17FF
 ;	1FE7 activates 1k of RAM instead (1000-13FF is write, 1400-17ff read)
-;	1FE8 - 1FEB activates which 256B RAM shows up at $1800/$1900
+;	1FE8 - 1FEB activates which of 4 256B RAM banks show up at $1800/$1900
 ;	1A00 - 1FDF ROM always visible
 
 ; by Vince `deater` Weaver <vince@deater.net>
