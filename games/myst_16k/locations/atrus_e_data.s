6b0c0eab63d460928468a7aa995f030c7d89e51d
Vince Weaver <vincent.weaver@maine.edu>
2023-06-28 16:57:29 -0400

    myst: working on getting more grab actions supported

--- games/myst_16k/locations/atrus_e_data.s
+++ games/myst_16k/locations/atrus_e_data.s
@@ -6,9 +6,9 @@
 .byte $00,$00			; background color, background color2
 .byte 4,$00			; overlay (sprite1) coarse/fine
 .byte 0,$00			; coarse/fine of missile0 (vertical line)
-.byte $00,$00			; XMAX/XMIN of grab area
-.byte $00,$00			; YMAX/YMIN of grab area
-.byte LOCATION_TRAPPED		; center destination
+.byte 34*2,48*2			; XMAX/XMIN of grab area
+.byte 16,30			; YMAX/YMIN of grab area
+.byte LOCATION_DNI_E		; center destination
 .byte $FF			; left destination
 .byte $FF			; right destination
 .byte $00,$00			; unused
