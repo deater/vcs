61fc6e8812351e9304373984747389be6f6154f6
Vince Weaver <vincent.weaver@maine.edu>
2023-07-28 16:19:29 -0400

    myst: more rom rearranging
    managed to free up 512 bytes or so in the end

--- games/myst_16k/bank5_clock/rom_bank5.s
+++ games/myst_16k/bank5_clock/rom_bank5.s
@@ -3,8 +3,6 @@
 
 steps_e_data_zx02:
 .incbin "steps_e_data.zx02"
-;shack_n_data_zx02:
-;.incbin "shack_n_data.zx02"
 library_up_data_zx02:
 .incbin "library_up_data.zx02"
 library_nw_data_zx02:
@@ -13,13 +11,9 @@ library_ne_data_zx02:
 .incbin "library_ne_data.zx02"
 clock_puzzle_data_zx02:
 .incbin "clock_puzzle_data.zx02"
-
-hint_data_zx02:
-.incbin "hint_data.zx02"
 elevator_s_data_zx02:
 .incbin "elevator_s_data.zx02"
-elevator_n_data_zx02:
-.incbin "elevator_n_data.zx02"
+
 
 .include "clock_update.s"
 .include "../fireplace_update.s"
