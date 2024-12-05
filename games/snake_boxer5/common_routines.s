c50145cb1e68b39c3625742a785112f7a4c3f8f0
Vince Weaver <vince@deater.net>
2024-12-05 00:49:52 -0500

    sb5: update title screen

---
+++ games/snake_boxer5/common_routines.s
@@ -0,0 +1,54 @@
+	;=====================
+	; VBLANK/VSYNC
+	;=====================
+	; our code takes 4 scanlines, clears out old one first
+	; this makes sure we get a full 3 scanlines of VSYNC
+
+common_vblank:
+
+	;============================
+	; Start Vertical Blank
+	;============================
+
+	lda	#2
+
+
+	;=================================
+	; wait for 3 scanlines of VSYNC
+	;=================================
+
+	sta	WSYNC		; wait until end of scanline
+	sta	VSYNC
+
+	sta	WSYNC
+	sta	WSYNC
+	lda	#0		; done beam reset			; 2
+	sta	WSYNC
+
+	; now in VSYNC scanline 3
+
+	sta	VSYNC							; 3
+
+	rts
+
+; 9 cycles in
+
+
+
+
+	;=============================
+	; overscan
+	;=============================
+	; amount of scanlines to wait is in X
+
+common_overscan:
+	lda	#$2		; turn off beam
+	sta	VBLANK
+
+common_delay_scanlines:
+	sta	WSYNC							; 3
+	dex								; 2
+	bne	common_delay_scanlines					; 2/3
+	rts								; 6
+
+; 10
--- games/snake_boxer5/snake_boxer5.s
+++ games/snake_boxer5/snake_boxer5.s
@@ -19,46 +19,37 @@ TEMP2	=	$91
 start:
 	sei		; disable interrupts
 	cld		; clear decimal bit
-	ldx	#$ff
-	txs		; point stack to top of zero page
 
-	; clear out the Zero Page (RAM and TIA registers)
+
+restart_game:
+
+	; init zero page and addresses to 0
 
 	ldx	#0
 	txa
 clear_loop:
-	sta	$0,X
-	inx
+	dex
+	txs
+	pha
 	bne	clear_loop
 
+	; S=$FF, A=$00, X=$00, Y=??
 
-start_frame:
+title_frame_loop:
 
-	; Start Vertical Blank
+	;============================================
+	; Start Vertical Blank (with one extra WSYNC)
+	;============================================
 
-	lda	#0			; turn on beam
-	sta	VBLANK
+	jsr	common_vblank
 
-	lda	#2			; reset beam to top of screen
-	sta	VSYNC
 
-	; wait for 3 scanlines of VSYNC
-
-	sta	WSYNC			; wait until end of scanline
-	sta	WSYNC
-	sta	WSYNC
-
-	lda	#0			; done beam reset
-	sta	VSYNC
-
-	;=============================
+	;============================
 	; 37 lines of vertical blank
 	;=============================
 
-
-.repeat 35
-	sta	WSYNC
-.endrepeat
+	ldx	#35
+	jsr	common_delay_scanlines
 
 	;=======================
 	; scanline 36 -- align sprite
@@ -89,30 +80,34 @@ scad_x:
 	lda	#NUSIZ_QUAD_SIZE
 	sta	NUSIZ0
 
-	lda	#$1C		; yellow		; set color
+	lda	#$0		; yellow		; set color
 	sta	COLUP0
 	sta	COLUPF
 
 	ldy	#0
-	sty	GRP0
+	sty	VBLANK		; re-enable VBLANK
+
+;	sty	GRP0
+
+
 
 	ldx	#0
 
-	inc	FRAME
-	lda	FRAME
-	and	#$20
-	bne	beak_closed
-beak_open:
-	lda	#<game_overlay
-	sta	INL
-	lda	#>game_overlay
-	jmp	beak_done
-beak_closed:
-	lda	#<game_overlay2
-	sta	INL
-	lda	#>game_overlay2
-beak_done:
-	sta	INH
+;	inc	FRAME
+;	lda	FRAME
+;	and	#$20
+;	bne	beak_closed
+;beak_open:
+;	lda	#<game_overlay
+;	sta	INL
+;	lda	#>game_overlay
+;	jmp	beak_done
+;beak_closed:
+;	lda	#<game_overlay2
+;	sta	INL
+;	lda	#>game_overlay2
+;beak_done:
+;	sta	INH
 
 
 	sta	WSYNC
@@ -126,8 +121,8 @@ beak_done:
 	; draw 192 lines
 	; need to race beam to draw other half of playfield
 
-game_over_loop:
-	lda	colors,Y		;				; 4+
+title_loop:
+	lda	left_colors,Y		;				; 4+
 	sta	COLUPF			; set playfield color		; 3
 ; 7
 	lda	playfield0_left,Y	;				; 4+
@@ -139,32 +134,30 @@ game_over_loop:
 	; must write by CPU 28 [GPU 84]
 
 ; 21
-
 	lda	playfield2_left,Y	;				; 4+
 	sta	PF2			;				; 3
 	; must write by CPU 38 [GPU 116]
 
 ; 28
-	lda	(INL),Y							; 5
-;	lda	game_overlay,X						; 4
-	sta	GRP0							; 3
-; 36
-
 	; at this point we're at 28 cycles
 	lda	playfield0_right,Y	;				; 4+
 	sta	PF0			;				; 3
 	; must write by CPU 49 [GPU 148]
-; 43
-;	nop								; 2
+; 35
 	lda	playfield1_right,Y	;				; 4+
 	sta	PF1			;				; 3
 	; must write by CPU 54 [GPU 164]
-; 50
+; 42
 	lda	$80							; 3
-;	nop								; 
 	lda	playfield2_right,Y	;				; 4+
 	sta	PF2			;				; 3
 	; must write by CPU 65 [GPU 196]
+
+; 52
+
+	lda	right_colors,Y		;				; 4+
+;	force 4-cycle store
+	sta	a:COLUPF						; 4
 
 ; 60
 	inx                                                             ; 2
@@ -179,8 +172,8 @@ done_iny:
                                                                 ; 11/11
 
 ; 71
-	cpx	#192							; 2
-	bne	game_over_loop						; 2/3
+	cpx	#192						; 2
+	bne	title_loop					; 2/3
 
 
 done_loop:
@@ -191,22 +184,36 @@ done_loop:
 	; overscan
 	;==========================
 
-	lda	#$2		; turn off beam
-	sta	VBLANK
+	ldx	#29
+	jsr	common_overscan
 
-	ldx	#0
-overscan_loop:
-	sta	WSYNC
-	inx
-	cpx	#30
-	bne	overscan_loop
 
-	jmp	start_frame
+	 ;============================
+        ; Overscan scanline 30
+        ;============================
+        ; check for button
+        ; we used to check for RESET too, but we'd need to debounce it
+        ;       and in theory it would be sorta pointless to RESET at title
+        ;============================
+
+waited_enough:
+        lda     INPT4                   ; check if joystick button pressed
+        bpl     done_title
+
+
+done_check_input:
+
+        jmp     title_frame_loop        ; bra
+
+done_title:
+
+        rts
+
 
 .align $100
 
-.include "game_over.inc"
-.include "game_overlay.inc"
+.include "title.inc"
+.include "common_routines.s"
 
 .segment "IRQ_VECTORS"
 	.word start	; NMI
