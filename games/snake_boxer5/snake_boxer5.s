f896056cf27bf036e86353ea9c4c57772399f8e8
Vince Weaver <vincent.weaver@maine.edu>
2024-12-05 14:51:29 -0500

    sb5: draw the snake

--- games/snake_boxer5/snake_boxer5.s
+++ games/snake_boxer5/snake_boxer5.s
@@ -119,8 +119,10 @@ scad_x:
 	;=============================================
 	;=============================================
 
+	;=============================================
 	; draw 112 lines of the title
 	; need to race beam to draw other half of playfield
+	;=============================================
 
 title_loop:
 	lda	left_colors,Y		;				; 4+
@@ -175,13 +177,143 @@ done_iny:
 ; 71
 	cpx	#112						; 2
 	bne	title_loop					; 2/3
+done_toploop:
+
+;	ldx	#80
+;	jsr	common_delay_scanlines
 
 
-	ldx	#80
-	jsr	common_delay_scanlines
 
-done_loop:
+	;===========================
+	;===========================
+	; 112 - 191 = snake sprite
+	;===========================
+	;===========================
+	; 48-pixel sprite!!!!
+	;
+draw_the_cheat:
+	;================
+	; scanline 112
+	;	set things up
 
+	lda	#$1A			; yellow
+	sta	COLUP0	; set sprite color
+	sta	COLUP1	; set sprite color
+
+	lda	#NUSIZ_THREE_COPIES_CLOSE
+	sta	NUSIZ0
+	sta	NUSIZ1
+
+	lda	#0		; turn off sprite
+	sta	GRP0
+	sta	GRP1
+	sta	HMP1			;			3
+
+	lda	#1		; turn on delay
+	sta	VDELP0
+	sta	VDELP1
+
+	sta	WSYNC
+
+	;=================
+	; scanline 81
+
+	; to center exactly would want sprite0 at
+	;	CPU cycle 41.3
+	; and sprite1 at
+	;	GPU cycle 44
+
+	ldx	#6		;				2
+cpad_x:
+	dex			;				2
+	bne	cpad_x		;				2/3
+	; 3 + 5*X each time through
+
+	lda	$80		; nop 6
+	lda	$80
+
+
+	; beam is at proper place
+	sta	RESP0						; 3
+	; 41 (GPU=123, want 124) +1
+	sta	RESP1						; 3
+	; 44 (GPU=132, want 132) 0
+
+	lda	#$F0		; opposite what you'd think
+	sta	HMP0			;			3
+;	lda	#$00
+;	sta	HMP1			;			3
+
+	sta	WSYNC
+	sta	HMOVE		; adjust fine tune, must be after WSYNC
+
+	ldx	#79		; init X
+	stx	TEMP2
+
+	;===============================
+	; scanline 82
+
+	jmp	over_align2
+.align $100
+over_align2:
+	sta	WSYNC
+
+	;================================
+	; scanline 83
+
+spriteloop_cheat:
+	; 0
+	lda	snake_sprite0,X	; load sprite data		; 4+
+	sta	GRP0			; 0->[GRP0] [GRP1 (?)]->GRP1	; 3
+	; 7
+	lda	snake_sprite1,X	; load sprite data		; 4+
+	sta	GRP1			; 1->[GRP1], [GRP0 (0)]-->GRP0	; 3
+	; 14
+	lda	snake_sprite2,X	; load sprite data		; 4+
+	sta	GRP0			; 2->[GRP0], [GRP1 (1)]-->GRP1	; 3
+	; 21
+
+	lda	snake_sprite5,X					; 4+
+	sta	TEMP1							; 3
+	; 28
+	lda	snake_sprite4,X					; 4+
+	tay								; 2
+	; 34
+	lda	snake_sprite3,X	;				; 4+
+	ldx	a:TEMP1			; force extra cycle		; 4
+	; 42
+
+	sta	GRP1			; 3->[GRP1], [GRP0 (2)]-->GRP0	; 3
+	; 45 (need this to be 44 .. 46)
+
+	sty	GRP0			; 4->[GRP0], [GRP1 (3)]-->GRP1	; 3
+	; 48 (need this to be 47 .. 49)
+	stx	GRP1			; 5->[GRP1], [GRP0 (4)]-->GRP0	; 3
+	; 51 (need this to be 50 .. 51)
+
+	sty	GRP0			; ?->[GRP0], [GRP1 (5)]-->GRP1 	; 3
+	; 54 (need this to be 52 .. 54)
+
+	; delay 11
+
+	inc	TEMP1	; 5
+	lda	TEMP1	; 3
+	lda	TEMP1	; 3
+
+
+	; 65
+
+	dec	TEMP2							; 5
+	ldx	TEMP2							; 3
+	bpl	spriteloop_cheat					; 2/3
+	; 76  (goal is 76)
+
+	ldy	#0		; clear out sprites
+	sty	GRP1
+	sty	GRP0
+	sty	GRP1
+
+	sta	WSYNC
 
 
 	;==========================
@@ -192,17 +324,17 @@ done_loop:
 	jsr	common_overscan
 
 
-	 ;============================
-        ; Overscan scanline 30
-        ;============================
-        ; check for button
-        ; we used to check for RESET too, but we'd need to debounce it
-        ;       and in theory it would be sorta pointless to RESET at title
-        ;============================
+	;============================
+	; Overscan scanline 30
+	;============================
+	; check for button
+	; we used to check for RESET too, but we'd need to debounce it
+	;       and in theory it would be sorta pointless to RESET at title
+	;============================
 
 waited_enough:
-        lda     INPT4                   ; check if joystick button pressed
-        bpl     done_title
+	lda	INPT4			; check if joystick button pressed
+	bpl	done_title
 
 
 done_check_input:
@@ -217,9 +349,13 @@ done_title:
 .align $100
 
 .include "title.inc"
+.include "snake_sprite.inc"
+
 .include "common_routines.s"
 
 .segment "IRQ_VECTORS"
 	.word start	; NMI
 	.word start	; RESET
 	.word start	; IRQ
+
+
