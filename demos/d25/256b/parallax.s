26e7002249a8168c62f4f1ebf295dbfaa518777d
Vince Weaver <vincent.weaver@maine.edu>
2025-01-22 14:47:59 -0500

    parallax: more size optimization

--- demos/d25/256b/parallax.s
+++ demos/d25/256b/parallax.s
@@ -22,6 +22,7 @@
 ;	$FB  = 250 bytes	remove extraneous initialization
 ;	$F4  = 244 bytes	move overscan around
 ;	$DF  = 223 bytes	merge overscan/vsync/vblank
+;	$D2  = 210 bytes	set sprite locations at same time
 
 	;=============================
 	; clear out mem / init things
@@ -80,12 +81,14 @@ tia_frame:
 	;
 	;
 
-	ldx	#64
+NUM_WSYNCS = 66
+
+	ldx	#NUM_WSYNCS
 delay_scanlines:
-	cpx	#(64-30)	; triggers at scanline 30
+	cpx	#(NUM_WSYNCS-30)	; triggers at scanline 30
 	beq	do_vsync
 skip1:
-	cpx	#(64-34)	; triggers at scanline 34?
+	cpx	#(NUM_WSYNCS-34)	; triggers at scanline 34?
 	bne	skip_vsync
 do_vsync:
 	sty	VSYNC
@@ -107,43 +110,62 @@ skip_vsync:
 	; VBLANK scanline 31 -- init
 	;================================================
 
-	; increment frame
-	;
+	; other init
 
-	inc	FRAMEL                                                  ; 5
-	bne	no_frame_oflo						; 2/3
-frame_oflo:
-        inc	FRAMEH                                                  ; 5
-no_frame_oflo:
+	lda	#$86		; medium blue				; 2
+	sta	COLUP0							; 3
+	sta	COLUP1		; color of sprite grid			; 3
+; 12
+	lda	#NUSIZ_QUAD_SIZE					; 2
+	sta	NUSIZ0		; make sprite grid large		; 3
+	sta	NUSIZ1							; 3
+; 20
+	lda	#CTRLPF_REF|CTRLPF_PFP	; reflect			; 2
+	sta	CTRLPF			; also playfield priority	; 3
+; 25
 
 	; setup sprite0/sprite1 xpos
+	; SPRITE0_X always 48	(39+7 ??)
+	lda	#$70		; +7					; 2
+	sta	HMP0		; fine adjust				; 3
+; 30
+	; SPRITE1_X always 82	(49+5 ??)
+	lda	#$50							; 2
+	sta	a:HMP1		; force 4 cycles			; 4
+; 36
+	;===============================================================
+	; set up sprite0/sprite1 to be at proper X position
+	;===============================================================
+	; sprite0 is always at 39 cycles
+	; sprite1 is always at 49 cycles
 
-;	lda	#48		; SPRITE0_X always 48			; 2
-;	lda	#3		; SPRITE0_X DIV 16 is 3
-;       sta	SPRITE0_X_COARSE					; 3
+; 36
+        sta     RESP0			; set sprite0 xpos at 39	; 3
+; 39
+	pha	; combine for nop7
+	pla
 
-	; apply fine adjust
-	lda	#$70
-	sta	HMP0							; 3
-
-	; SPRITE1_X=82 = $52
-; 31
-        ; spritex DIV 16 = 5
-
-;	lda	#$5
- ;       sta	SPRITE1_X_COARSE					; 3
-; 42
-	; apply fine adjust
-	lda	#$50
-	sta	HMP1							; 3
+;	nop
+;	nop
+;	lda	$80	; nop3
+; 46
+        sta     RESP1			; set sprite1 xpos at 49	; 3
+; 49
 
 	sta	WSYNC
+	sta	HMOVE		; finalize fine adjust
+
+
 
 	;=========================================
 	; scanline 32/33 : setup zigzag
 	;=========================================
 ; 0
 	; zigzag when music hits?
+	lda	#8
+	ldx	FRAMEH
+	cpx	#4
+	bcc	zigzag_start
 
 	ldx	MUSIC_HIT		; check if music hit		; 2
 	beq	zigzag_start		; if 
@@ -164,100 +186,20 @@ zigzag_loop:
 
 	sta	WSYNC
 
-	;=======================================================
-	; scanline 34: set up sprite0 to be at proper X position
-	;=======================================================
-	; value will be 0..9
-
-; 0
-
-;	ldx	a:SPRITE0_X_COARSE	; force 4-cycle version		; 4
-	ldx	#3
-	nop
-; 4
-
-	nop								; 2
-	nop								; 2
-
-; 8
-	inx                     ;                                       ; 2
-	inx                     ;                                       ; 2
-; 12 (want to be 12 here)
-
-
-	; X is 2..12 here
-par_pad_x:
-	dex                     ;                                       2
-	bne	par_pad_x           ;                                       2/3
-                                ;===========================================
-                                ;       5*(coarse_x+2)-1
-                                ; COARSE_X is 0..9 so
-				;   9 .. 54 cycles
-
-; up to 66
-        ; beam is at proper place
-        sta     RESP0                                                   ; 3
-; up to 69
-
-	sta	WSYNC
-
-
-	;=======================================================
-	; scanline 35: set up sprite1 to be at proper X position
-	;=======================================================
-
-; 0
-
-;	ldx	a:SPRITE1_X_COARSE	; force 4-cycle version		; 4
-	ldx	#5
-	nop
-; 4
-
-	nop								; 2
-	nop								; 2
-
-; 8
-	inx                     ;                                       ; 2
-	inx                     ;                                       ; 2
-; 12 (want to be 12 here)
-
-
-par_pad_x1:
-	dex			;					; 2
-	bne	par_pad_x1	;					; 2/3
-                                ;===========================================
-                                ;       5*(coarse_x+2)-1
-                                ; COARSE_X is 0..9 so
-				;   9 .. 54 cycles
-; up to 66
-        ; beam is at proper place
-	sta     RESP1                                                   ; 3
-; up to 69
-
-	sta	WSYNC
-	sta	HMOVE		; finalize fine adjust
-
-
-
 	;================================================
 	; VBLANK scanline 36 -- init
 	;================================================
 ; 3
-	; other init
 
-	lda	#$86							; 2
-	sta	COLUP0							; 3
-	sta	COLUP1		; color of sprite grid			; 3
+	; increment frame
+	inc	FRAMEL                                                  ; 5
+	bne	no_frame_oflo						; 2/3
+frame_oflo:
+        inc	FRAMEH                                                  ; 5
+no_frame_oflo:
 
-; 19
-	lda	#NUSIZ_QUAD_SIZE					; 2
-	sta	NUSIZ0		; make sprite grid large		; 3
-	sta	NUSIZ1							; 3
 
-; 27
-	lda	#CTRLPF_REF|CTRLPF_PFP	; reflect			; 2
-	sta	CTRLPF			; also playfield priority	; 3
-; 32
+
 
 
 	lda	FRAMEL
