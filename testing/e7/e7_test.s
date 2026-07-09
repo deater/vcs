489111db8110540beea90ea6d21029361efc2fad
Vince Weaver <vincent.weaver@maine.edu>
2026-07-09 12:50:36 -0400

    e7_test: start working on e7 ram/rom tester

---
+++ testing/e7/e7_test.s
@@ -0,0 +1,24 @@
+; e7_test for Atari 2600
+
+; this creates the final ROM image after segments were
+;	created separtely
+
+; For E7 bank-switched cartridge (16k ROM, 2k RAM)
+
+;	1FE0 - 1FE6 activates that bank of ROM into $1000-$17FF
+;	1FE7 activates 1k of RAM instead (1000-13FF is write, 1400-17ff read)
+;	1FE8 - 1FEB activates which of 4 256B RAM banks show up at $1800/$1900
+;	1A00 - 1FDF ROM always visible
+
+; by Vince `deater` Weaver <vince@deater.net>
+
+
+.incbin	"rom_bank0.bin"			; 2k
+.incbin	"rom_bank1.bin"			; 2k
+.incbin	"rom_bank2.bin"			; 2k
+.incbin	"rom_bank3.bin"			; 2k
+.incbin	"rom_bank4.bin"			; 2k
+.incbin	"rom_bank5.bin"			; 2k
+.incbin	"rom_bank6.bin"			; 2k
+.incbin	"zero_512.bin"			; 512 bytes
+.incbin	"main.bin"			; 1.5k
---
+++ testing/e7/main.s
@@ -0,0 +1,140 @@
+; e7_test for Atari 2600
+
+; For E7 bank-switched cartridge (16k ROM, 2k RAM)
+
+; by Vince `deater` Weaver <vince@deater.net>
+
+.include "../../vcs.inc"
+
+.include "zp.inc"
+
+.if 0
+.include "locations.inc"
+
+.include "locations/level_locations.inc"
+.include "rom_bank5_routines.inc"
+.include "rom_bank6_routines.inc"
+
+	;==============================
+	;==============================
+.endif
+
+e7_test:
+	sei		; disable interrupts
+	cld		; clear decimal bit
+
+restart_test:
+
+.if 0
+
+	; init zero page and addresses to 0
+
+	ldx	#0
+	txa
+clear_loop:
+	dex
+	txs
+	pha
+	bne	clear_loop
+
+	; S=$FF, A=$00, X=$00, Y=??
+
+	;==============================
+	; var init
+	;==============================
+
+	lda	#$F8
+	sta	LIBRARY_PAGE_MASK
+
+	;==============================
+	; Run intro
+	;==============================
+
+	;=============================
+	; first run title (MYST logo)
+
+	; title is now in BANK5
+
+	sta	E7_SET_BANK5		; title is in rom bank6
+	sta	E7_SET_256_BANK0	; not necessary?
+	jsr	do_title
+
+	;=============================
+	; then run cleft
+
+	; cleft is in BANK6
+
+	sta	E7_SET_BANK6		; cleft is in rom bank6
+	jsr	do_cleft
+
+	;==============================
+	; Show book (ROM bank 6)
+	;==============================
+	; this all happens in bank 6...
+
+;	ldy	#LOCATION_ARRIVAL_N
+;	sty	CURRENT_LOCATION
+;	sty	LINK_DESTINATION
+
+	; switch to bank 6
+;	sta	E7_SET_BANK6	; not necessary, already in bank6?
+
+;	jsr	book_common
+
+
+	;==========================
+	; DEBUG/debug
+	;	makes getting white page easier
+
+.if 0
+	dec	SWITCH_STATUS
+.else
+	nop
+	nop
+.endif
+
+
+	;==============================
+	; load in current level
+	;==============================
+load_new_level:
+;	jsr	load_level
+
+	; we inline it now to save 4 bytes
+
+	.include "load_level.s"
+
+	;===========================
+	;===========================
+	; main engine
+	;===========================
+	;===========================
+
+	.include "level_engine.s"
+
+	;===========================
+	; common routines
+	;===========================
+
+	.include "zx02_optim.s"
+
+	.include "level_data.inc"
+;	.include "load_level.s"
+	.include "position.s"
+	.include "common_routines.s"
+
+;	.include "sound_update.s"
+	.include "sfx_data.inc"
+.endif
+
+; e7 signature for MAME */
+; this is LDA $FFE5
+;.byte $ad, $e5, $ff
+
+.segment "BANKSWITCH"
+	.byte $00
+
+.segment "IRQ_VECTORS"
+	.word e7_test	; NMI
+	.word e7_test	; RESET
+	.byte $E7,$00	; IRQ
