83a740d59dcb664bc014ed43d06fdd5eb35b6cf7
Vince Weaver <vincent.weaver@maine.edu>
2026-07-28 14:21:35 -0400

    e7-2: memory actions work

--- testing/e7-2/memory_test.s
+++ testing/e7-2/memory_test.s
@@ -43,41 +43,9 @@ start_test_frame:
 ;	beq	done_new_test						; 2/3
 ; 11
 
-	; configure new test
 
 	lda	#0							; 2
-;	sta	NEW_TEST	; reset new_test			; 3
-;	sta	DONE_TEST	; reset done_test			; 3
-;	sta	EXPECTED_L	; reset counter				; 3
-;	sta	BAD_RESULT						; 3
-;	sta	COLUBK			; clear background to black
 
-; --
-;	lda	WHICH_TEST	; set upper value based on test		; 3
-;	tax			; in X for later			; 2
-;	asl								; 2
-;	asl								; 2
-;	asl								; 2
-;	asl								; 2
-;	sta	EXPECTED_H						; 3
-; 41
-
-;	lda	#$f		; reset button debounce			; 2
-;	sta	BUTTON_COUNTDOWN					; 3
-; 46
-	lda	#$ff		; clear out digits			; 2
-;	sta	DIGITS2							; 3
-;	sta	DIGITS3							; 3
-
-; 54
-;	lda	ROM_START	; setup where to start to read		; 3
-;	sta	WHICH_PAGE	; ROM is $1000, RAM is $1400		; 3
-; 60
-;	ldx	WHICH_TEST
-;	sta	E7_SET_BANK0,X	; start in BANK0?			; 5
-; 65
-
-done_new_test:
 ; 12 / 65
 	sta	WSYNC
 
@@ -239,19 +207,135 @@ done_new_test:
 	sta	WSYNC
 
 
-
-
-
-
-
 	;=============================================
 	;=============================================
 	; pad out end
 	;=============================================
 	;=============================================
 
-	ldx	#35
+	ldx	#27
 	jsr	repeat_wsync
+
+
+	;==============================
+	; button pressed (6 cycles?)
+	;==============================
+	; check for button being pressed
+	; + nothing pressed is 1 scanline
+	; + read		7 scanlines
+	; + write		1 scanlines
+
+
+	jsr	check_joypad_button
+
+	bcs	button_was_pressed
+
+	jmp	check_joypad_button_extra
+
+button_was_pressed:
+	; was pressed
+
+	ldx	WHICH_ROW
+	lda	button_table_h,X
+	pha
+	lda	button_table_l,X
+	pha
+	rts
+
+button_table_l:
+.byte <(button_row_0-1),<(button_row_1-1),<(button_row_2-1)
+.byte <(button_row_3-1),<(button_row_4-1),<(button_row_5-1)
+.byte <(button_row_6-1),<(button_row_7-1)
+
+button_table_h:
+.byte >(button_row_0-1),>(button_row_1-1),>(button_row_2-1)
+.byte >(button_row_3-1),>(button_row_4-1),>(button_row_5-1)
+.byte >(button_row_6-1),>(button_row_7-1)
+
+
+	; change bank
+button_row_0:
+	ldx	BANK_256
+	lda	$1FE8,X		; 1FE8=bank0,1fE9=1,1feA=2,1feb=3
+
+	jmp	check_joypad_button_none
+
+button_row_1:
+	ldx	BANK_1K
+	lda	$1FE0,X		; 1FE0=bank0 ROM.  1FE7=1k RAM bank
+
+	jmp	check_joypad_button_none
+
+button_row_2:
+	lda	READ_256L
+	sta	INL
+	lda	READ_256H
+	sta	INH
+	ldy	#0
+	lda	(INL),Y
+	sta	READ_VALUE
+
+	ldy	#3
+	jsr	update_byte
+
+	jmp	check_joypad_button_read
+
+button_row_3:
+	lda	WRITE_256L
+	sta	INL
+	lda	WRITE_256H
+	sta	INH
+	lda	WRITE_VALUE
+	ldy	#0
+	sta	(INL),Y
+
+	jmp	check_joypad_button_none
+
+
+button_row_4:
+	lda	READ_1KL
+	sta	INL
+	lda	READ_1KH
+	sta	INH
+	ldy	#0
+	lda	(INL),Y
+	sta	READ_VALUE
+
+	ldy	#3
+	jsr	update_byte
+
+	jmp	check_joypad_button_read
+
+button_row_5:
+	lda	WRITE_1KL
+	sta	INL
+	lda	WRITE_1KH
+	sta	INH
+	lda	WRITE_VALUE
+	ldy	#0
+	sta	(INL),Y
+
+	jmp	check_joypad_button_none
+
+button_row_6:
+button_row_7:
+	; do nothing
+	jmp	check_joypad_button_none
+
+check_joypad_button_extra:
+	sta	WSYNC
+
+
+check_joypad_button_none:
+
+	; delay 6 rows
+
+	ldx	#6
+	jsr	repeat_wsync
+
+check_joypad_button_read:
+
+	sta	WSYNC
 
 
 
@@ -263,8 +347,150 @@ done_new_test:
 	;==========================
 	;==========================
 
-	ldx	#27
+	ldx	#13
 	jsr	common_overscan
+
+
+	;==============================
+	; overscan 14-26 (13 scanlines)
+	;==============================
+	; check for right being pressed
+	; + nothing pressed is 1 scanline
+	; + 8-bit value =  7 lines
+	; + 16-bit value = 13 lines
+
+
+	jsr	check_joypad_right
+
+	bcs	right_was_pressed
+
+	jmp	check_joypad_right_none
+
+right_was_pressed:
+	; was pressed
+
+	ldx	WHICH_ROW
+	lda	right_table_h,X
+	pha
+	lda	right_table_l,X
+	pha
+	rts
+
+right_table_l:
+.byte <(right_row_0-1),<(right_row_1-1),<(right_row_2-1)
+.byte <(right_row_3-1),<(right_row_4-1),<(right_row_5-1)
+.byte <(right_row_6-1),<(right_row_7-1)
+
+right_table_h:
+.byte >(right_row_0-1),>(right_row_1-1),>(right_row_2-1)
+.byte >(right_row_3-1),>(right_row_4-1),>(right_row_5-1)
+.byte >(right_row_6-1),>(right_row_7-1)
+
+
+right_row_0:
+	inc	BANK_256
+	lda	BANK_256
+	and	#3
+	sta	BANK_256
+	ldy	#0
+	beq	check_joypad_right_8	; bra
+
+right_row_1:
+	inc	BANK_1K
+	lda	BANK_1K
+	and	#7
+	sta	BANK_1K
+	ldy	#1
+	bne	check_joypad_right_8	; bra
+
+right_row_2:
+	clc
+	lda	#1
+	adc	READ_256L
+	sta	READ_256L
+	lda	#0
+	adc	READ_256H
+	sta	READ_256H
+
+	ldy	#4
+	jsr	update_byte
+	ldy	#5
+	bne	check_joypad_right_16	; bra
+
+right_row_3:
+	clc
+	lda	#1
+	adc	WRITE_256L
+	sta	WRITE_256L
+	lda	#0
+	adc	WRITE_256H
+	sta	WRITE_256H
+
+	ldy	#6
+	jsr	update_byte
+	ldy	#7
+	bne	check_joypad_right_16	; bra
+
+right_row_4:
+	clc
+	lda	#1
+	adc	READ_1KL
+	sta	READ_1KL
+	lda	#0
+	adc	READ_1KH
+	sta	READ_1KH
+
+	ldy	#8
+	jsr	update_byte
+	ldy	#9
+	bne	check_joypad_right_16	; bra
+
+right_row_5:
+	clc
+	lda	#1
+	adc	WRITE_1KL
+	sta	WRITE_1KL
+	lda	#0
+	adc	WRITE_1KH
+	sta	WRITE_1KH
+
+	ldy	#10
+	jsr	update_byte
+	ldy	#11
+	bne	check_joypad_right_16	; bra
+
+right_row_6:
+	inc	WRITE_VALUE
+	ldy	#2
+	bne	check_joypad_right_8	; bra
+
+right_row_7:
+	; do nothing
+
+
+check_joypad_right_none:
+
+	; delay 13 rows
+
+	ldx	#13
+	jsr	repeat_wsync
+
+	jmp	check_joypad_right_done
+
+check_joypad_right_8:
+
+	; delay 6 rows to get to 13
+
+	ldx	#6
+	jsr	repeat_wsync
+
+check_joypad_right_16:
+	jsr	update_byte
+
+
+
+check_joypad_right_done:
+	sta	WSYNC
 
 	;==========================
 	; overscan 27
@@ -312,10 +538,8 @@ check_joypad_up_done:
 	;==========================
 	; check for button press
 
-	jsr	check_joypad_button
-	bcs	done_test
-
-;current_test_continues:
+;	jsr	check_joypad_button
+;	bcs	done_test
 
 	sta	WSYNC
 
@@ -324,32 +548,7 @@ check_joypad_up_done:
 
 	jmp	start_test_frame
 
-done_test:
 
-
-;	inc	NEW_TEST
-;	inc	WHICH_TEST
-
-;	lda	WHICH_TEST
-;	cmp	#8
-;	beq	done_roms
-
-
-;	cmp	#7
-;	bne	not_ram
-
-;	lda	#$14
-;	sta	ROM_START
-
-not_ram:
-
-	sta	WSYNC
-
-	jmp	start_test_frame
-
-
-done_roms:
-	rts
 
 
 	; handle row highlighting
